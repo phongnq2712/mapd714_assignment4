9a141a93108a5ba44eb45c7adaff5fe6da08c51c
phongnq <phongnq2712@gmail.com>
2021-11-13 11:16:26 -0500

    implement functions for layout

--- QuocPhongNgo_MAPD714_Assignment4/CustomTableViewCell.swift
+++ QuocPhongNgo_MAPD714_Assignment4/CustomTableViewCell.swift
@@ -1,14 +1,18 @@
-//
-//  CustomTableViewCell.swift
-//  week8
-//
-//  Created by Phong on 04/11/2021.
-//
+/**
+ * Assignment 4
+ * File Name:    CustomTableViewCell.swift
+ * Author:         Quoc Phong Ngo
+ * Student ID:   301148406
+ * Version:        1.0
+ * Date Created:   November 12th, 2021
+ */
 
 import UIKit
 
 class CustomTableViewCell: UITableViewCell {
 
+    weak var useButton: UIButton!
+    
     var name:String = "" {
         didSet {
             if(name != oldValue) {
@@ -16,42 +20,54 @@ class CustomTableViewCell: UITableViewCell {
             }
         }
     }
-    var genre:String = "" {
+    var dueDate:String = "" {
         didSet {
-            if(genre != oldValue)
+            if(dueDate != oldValue)
             {
-                genreLabel.text = genre
+                dueDateLabel.text = dueDate
             }
         }
     }
     var nameLabel: UILabel!
-    var genreLabel: UILabel!
+    var dueDateLabel: UILabel!
     
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
         // add a new subview for the new label
-        let nameLabelRect = CGRect(x:0, y:5, width: 70, height: 15)
-        let nameMarker = UILabel(frame: nameLabelRect)
-        nameMarker.textAlignment = NSTextAlignment.right
-        nameMarker.text = "Movie Name:"
-        nameMarker.font = UIFont.boldSystemFont(ofSize: 12)
-        contentView.addSubview(nameMarker)
+        //let nameLabelRect = CGRect(x:0, y:5, width: 70, height: 15)
+        //let nameMarker = UILabel(frame: nameLabelRect)
+        //nameMarker.textAlignment = NSTextAlignment.right
+        //nameMarker.text = "Movie Name:"
+        //nameMarker.font = UIFont.boldSystemFont(ofSize: 12)
+        //contentView.addSubview(nameMarker)
         
         // add a new subview for the new label
-        let genreLabelRect = CGRect(x:0, y:26, width: 70, height: 15)
-        let genreMarker = UILabel(frame: genreLabelRect)
-        genreMarker.textAlignment = NSTextAlignment.right
-        genreMarker.text = "Genre Name:"
-        genreMarker.font = UIFont.boldSystemFont(ofSize: 12)
-        contentView.addSubview(genreMarker)
+        //let genreLabelRect = CGRect(x:0, y:26, width: 70, height: 15)
+        //let genreMarker = UILabel(frame: genreLabelRect)
+        //genreMarker.textAlignment = NSTextAlignment.right
+        //genreMarker.text = "Genre Name:"
+        //genreMarker.font = UIFont.boldSystemFont(ofSize: 12)
+        //contentView.addSubview(genreMarker)
         
-        let nameValueRect = CGRect(x:80, y:5, width: 200, height: 15)
+        let nameValueRect = CGRect(x:80, y:0, width: 200, height: 40)
         nameLabel = UILabel(frame: nameValueRect)
         contentView.addSubview(nameLabel)
         
-        let genreValueRect = CGRect(x:80, y:26, width: 200, height: 15)
-        genreLabel = UILabel(frame: genreValueRect)
-        contentView.addSubview(genreLabel)
+        let genreValueRect = CGRect(x:80, y:16, width: 200, height: 40)
+        dueDateLabel = UILabel(frame: genreValueRect)
+        contentView.addSubview(dueDateLabel)
+        
+        //Initialize Use button
+        let useButton = UIButton(frame: CGRect(x: 8, y: 4.5, width: 48, height: 28))
+        self.useButton = useButton
+        //super.init(style: .default, reuseIdentifier: "moviesTable")
+        self.frame = frame
+        
+        //Setup Use button
+        addSubview(useButton)
+        useButton.setTitle("Edit", for: .normal)
+        useButton.setTitleColor(.blue, for: .normal)
+        useButton.center.y = self.center.y
     }
     
     required init?(coder: NSCoder) {
--- QuocPhongNgo_MAPD714_Assignment4/ViewController.swift
+++ QuocPhongNgo_MAPD714_Assignment4/ViewController.swift
@@ -1,32 +1,38 @@
-//
-//  ViewController.swift
-//  QuocPhongNgo_MAPD714_Assignment4
-//
-//  Created by Phong on 07/11/2021.
-//
+/**
+ * Assignment 4
+ * File Name:    ViewController.swift
+ * Author:         Quoc Phong Ngo
+ * Student ID:   301148406
+ * Version:        1.0
+ * Date Created:   November 12th, 2021
+ */
 
 import UIKit
 
-class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
-//    @IBOutlet var tableView: UITableView!
+class ViewController: UIViewController {
     
     @IBOutlet weak var tableView: UITableView!
     private let movies = [
-        ["Name":"Dune 2021","Genre":"Sci-Fi"],
-        ["Name":"Shang Chi","Genre":"Superhero"],
-        ["Name":"Star Wars","Genre":"Sci-Fi"],
-        ["Name":"Blade Runner","Genre":"Sci-Fi"],
-        ["Name":"Spiderman","Genre":"Sci-Fi"],
-        ["Name":"The Avengers","Genre":"Sci-Fi"],
-        ["Name":"Back to the Future","Genre":"Sci-Fi"]
+        ["Name":"Clean room","DueDate":"November 14, 2021"],
+        ["Name":"Wipe floor","DueDate":"November 15, 2021"],
+        ["Name":"Water plant","DueDate":"November 16, 2021"],
+        ["Name":"Vacuum carpets","DueDate":"November 17, 2021"],
+        ["Name":"Dust furniture","DueDate":"November 18, 2021"],
+        ["Name":"Spot clean cabinet fronts","DueDate":"November 19, 2021"],
+        ["Name":"Clean kitchen table","DueDate":"November 20, 2021"]
     ]
     let tableIdentifier = "moviesTable"
 
     override func viewDidLoad() {
         super.viewDidLoad()
         tableView.register(CustomTableViewCell.self, forCellReuseIdentifier: tableIdentifier)
-        // Do any additional setup after loading the view.
     }
+      
+}
+
+extension ViewController: UITableViewDataSource
+{
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return movies.count
     }
@@ -34,29 +40,59 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         // Step 1 - Instantiate an object of type UITableViewCell
         var cell = tableView.dequeueReusableCell(withIdentifier: tableIdentifier, for: indexPath) as! CustomTableViewCell
-        let rowData = movies[indexPath.row]
+        var rowData = movies[indexPath.row]
         cell.name = rowData["Name"]!
-        cell.genre = rowData["Genre"]!
+        if(indexPath.row < 2)
+        {
+            cell.backgroundColor = UIColor.systemGray5
+            cell.dueDate = "Completed"
+        }
+        
+        //let editImage = UIImage(systemName: "square.and.pencil")
+        //cell.imageView?.image = editImage
+
+        // Add Edit button target (add target only once when the cell is created)
+        cell.useButton.addTarget(self, action: #selector(pressButton), for: .touchUpInside)
+        cell.useButton.tag = indexPath.row
         
         // switch view
         let switchView = UISwitch(frame: .zero)
         switchView.setOn(false, animated: true)
         switchView.tag = indexPath.row
-        switchView.addTarget(self, action: #selector(self.switchChanged(_:)), for: .valueChanged)
+        switchView.addTarget(self, action: #selector(switchChanged(_:)), for: .valueChanged)
         cell.accessoryView = switchView
-//        if(cell == nil)
-//        {
-//            cell = UITableViewCell(style: UITableViewCell.CellStyle.default, reuseIdentifier: tableIdentifier)
-//        }
-        
+                
         return cell
     }
     
-    @objc func switchChanged(_ sender: UISwitch!)
-    {
-        print("Table row switch Changed \(sender.tag)")
-        print("The switch is \(sender.isOn ? "ON" : "OFF")")
+    /**
+     * Selector for pressing button Edit
+     */
+    @objc func pressButton(_ sender: UIButton) {
+         //Somehow get the IndexPath of the row whose button called this function
+        print("Movie is \(movies[sender.tag])")
+        if let vc = storyboard?.instantiateViewController(identifier: "DetailViewController") as? DetailViewController {
+            navigationController?.pushViewController(vc, animated: true)
+        }
     }
+    
+    /**
+     * Selector for changing value of switch 'Due Date'
+     */
+    @objc func switchChanged(_ sender: UISwitch)
+    {
+        // get indexPath for current cell
+        let indexPath = self.tableView.indexPathForSelectedRow
+        
+        let cell = self.tableView.cellForRow(at: indexPath!) as! CustomTableViewCell
 
+        var rowData = movies[sender.tag]
+        if(sender.isOn)
+        {
+            cell.dueDate = rowData["DueDate"]!
+        } else {
+            cell.dueDate = ""
+        }
+        self.tableView.cellForRow(at: indexPath!)
+    }
 }
-
